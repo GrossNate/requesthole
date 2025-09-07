dd505124a35e13fd50c54564a81a067714501cc0
Nathan Gross <nathan.gross@gmail.com>
2025-09-07 18:28:33 -0500

    chore: Removed extraneous logging.

--- requesthole_backend/src/RequestBroadcaster.ts
+++ requesthole_backend/src/RequestBroadcaster.ts
@@ -11,21 +11,17 @@ class RequestBroadcaster {
   addClient(holeAddress: string, reply: FastifyReply) {
     if (!this.holes.has(holeAddress)) {
       this.holes.set(holeAddress, new Set<FastifyReply>());
-      console.log("Hole set added");
     }
     this.holes.get(holeAddress)?.add(reply);
-    console.log("Hole listener added");
   }
 
   deleteClient(holeAddress: string, reply: FastifyReply) {
     this.holes.get(holeAddress)?.delete(reply);
-    console.log("Hole listener deleted");
   }
 
   broadcastRequest(holeAddress: string, request: RequestSansBody) {
     this.holes.get(holeAddress)?.forEach((reply) => {
       reply.sse({ data: JSON.stringify(request) });
-      console.log("Broadcast sent");
     });
   }
 }
